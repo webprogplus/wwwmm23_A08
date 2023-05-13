60c71bb5f394b5264a44e753ebace2f136f4ca03
Rudi Schraml <rudi.schraml@gmail.com>
2023-05-13 20:09:12 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+Flashcards
+
+This is the starter code the Flashcards assignment. There is a fair amount of starter code this time around, so please check out the instructions on the webseite for an explanation of the files.
---
+++ constants.js
@@ -0,0 +1,28 @@
+const FLASHCARD_DECKS = [{
+  title: 'Port Numbers',
+  words: {
+    'SSH': '22',
+    'HTTP': '80',
+    'HTTPS': '443',
+    'SMTP': '25',
+    'FTP': '21'
+  }
+}, {
+  title: 'HTTP Status Codes
+    '404': 'Not found',
+    'OK': '200',
+    '500': 'Internal Server Error',
+    'Permanent Redirect': '301'
+  }
+}, {
+  title: 'Your Deck',
+  words: {
+    '1': '?',
+    '2': '?',
+    '3': '?',
+    '4': '?',
+    '5': '?',
+    '6': '?',
+    '7': '?'
+  }
+}];
